a86f750835cd91b864b767714020367bae2ca23f
iamyol4 <yolasahrani@gmail.com>
2025-05-29 17:15:19 +0700

    file from database to json

--- latihan1.php
+++ latihan1.php
@@ -1,18 +1,28 @@
 <?php
 
- $mahasiswa =[
-   [
-         "nama" => "Yola Sahrani",
-          "nim" => "2217020134",
-          "email"=> "yolasahrani@gmail.com"
-     ],
-     [
-          "nama"=> "Nita Herawaty",
-          "nim" => "25170120123",
-          "email"=> "nitahera@gmail.com"
+//  $mahasiswa =[
+//    [
+//          "nama" => "Yola Sahrani",
+//           "nim" => "2217020134",
+//           "email"=> "yolasahrani@gmail.com"
+//      ],
+//      [
+//           "nama"=> "Nita Herawaty",
+//           "nim" => "25170120123",
+//           "email"=> "nitahera@gmail.com"
     
-     ]
- ];
- $data = json_encode($mahasiswa);
- echo $data;
+//      ]
+//  ];
+//  $data = json_encode($mahasiswa);
+//  echo $data;
+
+
+$dbh = new PDO("mysql:host=localhost;dbname=e-monti","root","");
+$db =$dbh->prepare("SELECT * FROM building");
+$db->execute();
+$building =$db->fetchAll(PDO::FETCH_ASSOC);
+
+
+$data = json_encode($building); 
+echo $data; 
  ?>
