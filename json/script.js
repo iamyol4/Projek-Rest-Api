e6fbd0baa8889eb6c227feca4e8f5de71acca737
iamyol4 <yolasahrani@gmail.com>
2025-05-29 17:32:16 +0700

    json to objek

---
+++ json/script.js
@@ -0,0 +1,18 @@
+ let mahasiswa = {
+     nama: "Yola Sahrani",
+     nim : "2217020134",
+    email : "yolasahrani@gmail.com"
+    
+ }
+ console.log(JSON.stringify(mahasiswa)); 
+
+ let xhr = new XMLHttpRequest();
+ xhr.onreadystatechange = function () {
+     if (xhr.readyState == 4 && xhr.status == 200){
+         let mahasiswa = JSON.parse(this.responseText);
+         console.log(mahasiswa);
+
+     }
+ }
+ xhr.open('GET','coba.json',true);
+xhr.send();
