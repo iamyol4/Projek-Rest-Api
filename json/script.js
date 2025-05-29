b3f4e9e1366fce41ec12f7ed1043057a9884ec26
iamyol4 <yolasahrani@gmail.com>
2025-05-29 17:37:15 +0700

    use jQuery

--- json/script.js
+++ json/script.js
@@ -1,18 +1,21 @@
- let mahasiswa = {
-     nama: "Yola Sahrani",
-     nim : "2217020134",
-    email : "yolasahrani@gmail.com"
+//  let mahasiswa = {
+//      nama: "Yola Sahrani",
+//      nim : "2217020134",
+//     email : "yolasahrani@gmail.com"
     
- }
- console.log(JSON.stringify(mahasiswa)); 
+//  }
+//  console.log(JSON.stringify(mahasiswa)); 
 
- let xhr = new XMLHttpRequest();
- xhr.onreadystatechange = function () {
-     if (xhr.readyState == 4 && xhr.status == 200){
-         let mahasiswa = JSON.parse(this.responseText);
-         console.log(mahasiswa);
+//  let xhr = new XMLHttpRequest();
+//  xhr.onreadystatechange = function () {
+//      if (xhr.readyState == 4 && xhr.status == 200){
+//          let mahasiswa = JSON.parse(this.responseText);
+//          console.log(mahasiswa);
 
-     }
- }
- xhr.open('GET','coba.json',true);
-xhr.send();
+//      }
+//  }
+//  xhr.open('GET','coba.json',true);
+// xhr.send();
+$.getJSON('coba.json',function (data) {
+    console.log(data);
+});
