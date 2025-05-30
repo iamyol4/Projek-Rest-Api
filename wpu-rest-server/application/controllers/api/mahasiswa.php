d531a5a066084d310bfc3173a411c745b87dd878
iamyol4 <yolasahrani@gmail.com>
2025-05-30 23:06:20 +0700

    get data mahasiswa

--- wpu-rest-server/application/controllers/api/mahasiswa.php
+++ wpu-rest-server/application/controllers/api/mahasiswa.php
@@ -1,3 +1,13 @@
 <?php
+use Restserver\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
 
 class Mahasiswa extends REST_controler;
+{
+  public function  index_get()
+}
