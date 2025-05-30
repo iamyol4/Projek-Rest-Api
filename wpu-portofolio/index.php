b38144fd24c64d2fdd9128509c0d082898469e66
iamyol4 <yolasahrani@gmail.com>
2025-05-30 22:38:01 +0700

    konfigurasi database autoload

--- wpu-portofolio/index.php
+++ wpu-portofolio/index.php
@@ -48,7 +48,7 @@ $latestVideoId =$result['items'][0]['id']['videoId'];
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#home">Sandhika Galih</a>
+        <a class="navbar-brand" href="#home">Yola Sahrani</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -73,8 +73,8 @@ $latestVideoId =$result['items'][0]['id']['videoId'];
       <div class="container">
         <div class="text-center">
           <img src="img/profile1.png" class="rounded-circle img-thumbnail">
-          <h1 class="display-4">Sandhika Galih</h1>
-          <h3 class="lead">Lecturer | Programmer | Youtuber</h3>
+          <h1 class="display-4">Yola Sahrani</h1>
+          <h3 class="lead">Mahasiswa</h3>
         </div>
       </div>
     </div>
@@ -116,6 +116,7 @@ $latestVideoId =$result['items'][0]['id']['videoId'];
               <div class="col-md-8">
                 <h5><?= $channelName;?></h5>
                 <p><?= $subscriber;?> Subscribers."</p>
+                <div class="g-ytsubscribe" data-channelid="UCLkAepWjdylmXSltofFvsYQ" data-layout="default" data-count="default"></div>
               </div>
             </div>
             <div class="row mt-3 pb-3">
@@ -289,7 +290,7 @@ $latestVideoId =$result['items'][0]['id']['videoId'];
       <div class="container">
         <div class="row">
           <div class="col text-center">
-            <p>Copyright &copy; 2018.</p>
+            <p>Copyright &copy; 2025</p>
           </div>
         </div>
       </div>
@@ -306,5 +307,7 @@ $latestVideoId =$result['items'][0]['id']['videoId'];
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <script src="https://apis.google.com/js/platform.js"></script>
+
   </body>
 </html>
