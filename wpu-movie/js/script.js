988f9259a56b6b5cba3a334087f2b85b5c15bd25
iamyol4 <yolasahrani@gmail.com>
2025-05-30 11:37:45 +0700

    untuk menampilkan see detail

--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -1,5 +1,6 @@
 function searchMovies(){
-$('#search-button').on('click', function (){
+    $('#Movie-list').html('');
+
     $.ajax({
         url: 'http://omdbapi.com',
         type: 'get',
@@ -10,7 +11,8 @@ $('#search-button').on('click', function (){
             
         },
         success: function (result) {
-            if(result.Response == "True") {  let movies = result.Search;
+            if(result.Response == "True") {
+                let movies = result.Search;
 
                 $.each(movies, function(i,data){
                     $('#Movie-list').append(`<div class="card mb-3" style="width: 20rem;">
@@ -23,19 +25,25 @@ $('#search-button').on('click', function (){
                     </div>
                   </div>`);
 
-                });
-                $('#search-input').val('');
-   
-            } else {
-                $('#Movie-list').html(`<div class="col-12 text-center">
-                <h1 class = "text-center">`+ result.Error +`</h1></div>
-                `); 
-           }
-        }
-    });
 
-   $('#search-button').on('click', function (){
+               });
+
+               $('#search-input').val('');
+
+
+            } else {
+                 $('#Movie-list').html(`<div class="col-12 text-center">
+                 <h1 class = "text-center">`+ result.Error +`</h1></div>
+                 `); 
+            }
+        }
+
+    });
+}
+$('#search-button').on('click', function (){
     searchMovies();
+   
+
 });
 
 $('#search-input').on('keyup', function (e){
@@ -43,4 +51,40 @@ $('#search-input').on('keyup', function (e){
         searchMovies();
     }
 });
-}
+
+$('#Movie-list').on('click','.see-detail', function (){
+    console.log($(this).data('id'));
+
+    $.ajax({
+        url:'https://omdbapi.com',
+        dataType:'json',
+        data: {
+            'apikey':'14694818',
+            'i':$(this).data('id') 
+        },
+        success: function(movie){
+            if(movie.Response === "True"){
+
+                $('.modal-body').html(`<div class="container-fluid">
+                <div class="row">
+                <div class="col-md-4">
+                <img src="`+ movie.Poster +`"class="img-fluid">
+                </div>
+                <div class="col-md-8">
+                <ul class="list-group">
+                <li class="list-group-item"><h3> `+ movie.Title +`</h3></li>
+                <li class="list-group-item">Released : `+ movie.Released +`</li>
+                <li class="list-group-item">Director: `+ movie.Director +`</li>
+                <li class="list-group-item">Actors: `+ movie.Actors +`</li>
+                
+        
+                </ul>
+                </div>
+                </div>
+                `);
+            
+            }
+
+        }
+    })
+});
